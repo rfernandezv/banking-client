1a380062156331cfe583192b377ad041d5f7a76e
rfernandezv <richar.fernandezv@gmail.com>
2018-07-07 20:26:47 -0500

    Subir

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   apiUrl : //'https://java-banking-api.cfapps.io/api/' 
-           'http://netcore-banking-api.cfapps.io/api/'
+           'https://netcore-banking-api.cfapps.io/api/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   apiUrl : //'https://java-banking-api.cfapps.io/api/' 
-            'http://netcore-banking-api.cfapps.io/api/'
+            'https://netcore-banking-api.cfapps.io/api/'
 };
